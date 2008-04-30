7a32aeef9e5d258788e4798b3cae40212a16952c
Kerry Buckley <kerryjbuckley@gmail.com>
2008-04-30 21:51:25 +0100

    Added Blueprint CSS. Now the small matter of using them...

--- header.php
+++ header.php
@@ -6,7 +6,10 @@
 
 <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
 
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/stylesheets/blueprint/screen.css" type="text/css" media="screen, projection" />
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>stylesheets/blueprint/print.css" type="text/css" media="print" /> 
+<!--[if IE]><link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>stylesheets/blueprint/ie.css" type="text/css" media="screen, projection" /><![endif]-->
+
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
